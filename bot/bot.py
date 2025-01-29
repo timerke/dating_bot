2b17118df3d1f1c939fc9fcb8da350a851211d5a
timerke <timerke@mail.ru>
2025-01-29 11:31:47 +0300

    Added profile menu

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,6 @@
+from typing import Optional
 import telebot
-from . import utils as ut
-from .database import create_database
+from . import database as db, utils as ut
 
 
 class Bot:
@@ -12,15 +12,68 @@ class Bot:
         token = ut.get_token("config.ini")
         self._bot: telebot.TeleBot = telebot.TeleBot(token)
         self._add_handlers()
-        create_database()
+        db.create_database()
 
     def _add_handlers(self) -> None:
-        self._bot.add_message_handler({"function": self._start,
-                                       "filters": {"commands": ["start"]}
-                                       })
+        self._bot.register_message_handler(self._register_user, regexp="Зарегистрироваться")
+        self._bot.register_message_handler(self._start, commands=["start"])
+        self._bot.register_message_handler(self._show_profile, regexp="Профиль")
+        self._bot.register_message_handler(self._show_profile_photo, regexp="Фото")
 
-    def _start(self, message: telebot.types.Message) -> None:
-        print(type(message))
+    def _ask_user_to_register(self, message: telebot.types.Message):
+        markup = telebot.types.ReplyKeyboardMarkup()
+        markup.add(telebot.types.KeyboardButton("Зарегистрироваться"))
+        return self._bot.send_message(message.chat.id, "Хотите зарегистрироваться?", reply_markup=markup)
+
+    @staticmethod
+    def _get_user_id(message: telebot.types.Message) -> int:
+        """
+        :param message: message from user.
+        :return: user ID in Telegram.
+        """
+
+        return message.from_user.id
+
+    def _register_user(self, message: telebot.types.Message):
+        user_id = self._get_user_id(message)
+        if db.check_if_user_registered(user_id):
+            return self._show_main_menu(message, "Вы уже зарегистрированы.")
+
+        db.register_user(user_id, message.from_user.username)
+        return self._show_main_menu(message, "Вы зарегистрированы.")
+
+    def _show_main_menu(self, message: telebot.types.Message, text: Optional[str] = None):
+        markup = telebot.types.ReplyKeyboardMarkup()
+        markup.add(*[telebot.types.KeyboardButton("Анкеты"),
+                     telebot.types.KeyboardButton("Профиль"),
+                     telebot.types.KeyboardButton("Совпадения")])
+        text = text or "Главное меню"
+        return self._bot.send_message(message.chat.id, text, reply_markup=markup)
+
+    def _show_profile(self, message: telebot.types.Message):
+        markup = telebot.types.ReplyKeyboardMarkup()
+        markup.add(telebot.types.KeyboardButton("Имя"))
+        markup.add(telebot.types.KeyboardButton("Фамилия"))
+        markup.add(telebot.types.KeyboardButton("Возраст"))
+        markup.add(telebot.types.KeyboardButton("Пол"))
+        markup.add(telebot.types.KeyboardButton("Фото"))
+        markup.add(telebot.types.KeyboardButton("Возраст партнера"))
+        return self._bot.send_message(message.chat.id, "Профиль", reply_markup=markup)
+
+    def _show_profile_photo(self, message: telebot.types.Message):
+        markup = telebot.types.ReplyKeyboardMarkup()
+        markup.add(telebot.types.KeyboardButton("Добавить/заменить фото"))
+        photo = db.get_photo(self._get_user_id(message))
+        if photo is None:
+            return self._bot.send_message(message.chat.id, "У Вас нет фото.", reply_markup=markup)
+
+        return self._bot.send_photo(message.chat.id, photo, "Ваше фото")
+
+    def _start(self, message: telebot.types.Message):
+        if not db.check_if_user_registered(message.from_user.id):
+            return self._ask_user_to_register(message)
+
+        return self._show_main_menu(message)
 
     def run(self) -> None:
         self._bot.infinity_polling()
--- bot/database.py
+++ bot/database.py
@@ -60,11 +60,51 @@ def create_users_table(cursor) -> None:
     cursor.execute("CREATE TABLE IF NOT EXISTS users("
                    "user_tg_id INTEGER PRIMARY KEY,"
                    "username TEXT,"
-                   "first_name TEXT,"
-                   "last_name TEXT,"
-                   "date_of_birth TIMESTAMP,"
-                   "gender TEXT,"
-                   "age_min INT,"
-                   "age_max INT,"
-                   "biography TEXT,"
-                   "date TIMESTAMP)")
+                   "first_name TEXT DEFAULT NULL,"
+                   "last_name TEXT DEFAULT NULL,"
+                   "date_of_birth TIMESTAMP DEFAULT NULL,"
+                   "gender TEXT DEFAULT NULL,"
+                   "age_min INT DEFAULT NULL,"
+                   "age_max INT DEFAULT NULL,"
+                   "biography TEXT DEFAULT NULL)")
+
+
+def check_if_user_registered(user_tg_id: int) -> bool:
+    """
+    :param user_tg_id: user ID in Telegram.
+    :return: True if user was registered.
+    """
+
+    conn = sqlite3.connect(DATABASE_PATH)
+    cursor = conn.cursor()
+    cursor.execute("SELECT user_tg_id FROM users WHERE user_tg_id = ?", (user_tg_id, ))
+    user = cursor.fetchone()
+    conn.close()
+    return user is not None
+
+
+def get_photo(user_tg_id: int) -> bytes:
+    """
+    :param user_tg_id: user ID in Telegram.
+    :return: photo of user.
+    """
+
+    conn = sqlite3.connect(DATABASE_PATH)
+    cursor = conn.cursor()
+    cursor.execute("SELECT photo FROM photos WHERE user_tg_id = ?", (user_tg_id,))
+    photo = cursor.fetchone()
+    conn.close()
+    return photo
+
+
+def register_user(user_tg_id: int, username: str) -> None:
+    """
+    :param user_tg_id: user ID in Telegram;
+    :param username: username in Telegram.
+    """
+
+    conn = sqlite3.connect(DATABASE_PATH)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO users (user_tg_id, username) VALUES (?, ?)", (user_tg_id, username))
+    conn.commit()
+    conn.close()
